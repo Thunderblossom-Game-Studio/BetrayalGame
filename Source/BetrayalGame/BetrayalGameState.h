b740ae226526294164dcb3a6ce747cd8ae031c40
Joseph <jwlivingstone2002@gmail.com>
2024-02-07 00:38:18 +0000

    Starting stuff

---
+++ Source/BetrayalGame/BetrayalGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BetrayalGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETRAYALGAME_API ABetrayalGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
