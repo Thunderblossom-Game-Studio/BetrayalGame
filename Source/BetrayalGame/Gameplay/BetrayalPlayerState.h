eb3f9437acfc0af68480856c5d6c18a4af7a9bd4
Vasco Almeida <l.vasco.m.a21@gmail.com>
2024-02-21 19:57:52 +0000

    Game and Player state

---
+++ Source/BetrayalGame/Gameplay/BetrayalPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "BetrayalPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETRAYALGAME_API ABetrayalPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
