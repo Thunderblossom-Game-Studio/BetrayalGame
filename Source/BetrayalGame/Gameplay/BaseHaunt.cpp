cea376dac12c6c9d06e104afc85f8af76f68a27a
Vasco Almeida <l.vasco.m.a21@gmail.com>
2024-03-03 17:58:02 +0000

    Adding data field to UBaseHaunt

--- Source/BetrayalGame/Gameplay/BaseHaunt.cpp
+++ Source/BetrayalGame/Gameplay/BaseHaunt.cpp
@@ -3,3 +3,26 @@
 
 #include "BaseHaunt.h"
 
+#include "Factories/BlueprintFactory.h"
+#include "Kismet2/KismetEditorUtilities.h"
+
+void UBaseHaunt::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	UObject::GetLifetimeReplicatedProps(OutLifetimeProps);
+}
+
+
+void UBaseHaunt::StartHaunt()
+{
+	OnHauntStart();
+}
+
+void UBaseHaunt::OnTraitorPicked_Implementation(APlayerCharacter* TraitorPlayer)
+{
+
+}
+
+void UBaseHaunt::OnHauntEnd_Implementation()
+{
+	
+}
--- Source/BetrayalGame/Gameplay/BaseHaunt.h
+++ Source/BetrayalGame/Gameplay/BaseHaunt.h
@@ -3,15 +3,84 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PlayerCharacter.h"
 #include "UObject/NoExportTypes.h"
 #include "BaseHaunt.generated.h"
 
-/**
- * 
- */
+UENUM()
+enum EHauntCategory
+{
+	Hc_Asymmetric UMETA(DisplayName = "Asymmetric"),
+	Hc_HiddenAsymmetric UMETA(DisplayName = "Hidden Asymmetric"),
+	Hc_FreeForAll UMETA(DisplayName = "Free For All"),
+};
+
+
+
 UCLASS(Blueprintable)
 class BETRAYALGAME_API UBaseHaunt : public UObject
 {
 	GENERATED_BODY()
+
+private:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Haunt")
+	FName HauntName;
+	
+	UPROPERTY(EditAnywhere, Category = "Haunt", meta = (MultiLine = true));
+	FText HauntDescription;
+	
+	UPROPERTY(EditAnywhere, Category = "Haunt")
+	TEnumAsByte<EHauntCategory> HauntCategory;
+	
+	UPROPERTY(EditAnywhere, Category = "Haunt")
+	bool bHasTimer;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Haunt", meta = (EditCondition = "bHasTimer"))
+	float HauntDuration = 0.0f;
+
+#pragma region Traitor Properties
+	UPROPERTY(VisibleAnywhere, Category = "Haunt|Traitor")
+	bool bHasTraitor;
+	
+	UPROPERTY(EditAnywhere,
+			BlueprintReadOnly, Category = "Haunt|Traitor",
+			meta = (EditCondition = "HauntCategory == EHauntCategory::Hc_Asymmetric || HauntCategory == EHauntCategory::Hc_HiddenAsymmetric",
+					Tooltip = "Traitors only have an objective if the haunt is Asymmetric or Hidden Asymmetric"))
+	FDataTableRowHandle TraitorObjective;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Haunt|Traitor",
+			meta = (EditCondition = "HauntCategory == EHauntCategory::Hc_Asymmetric || HauntCategory == EHauntCategory::Hc_HiddenAsymmetric",
+					Tooltip = "Traitors only have a list of monsters if the haunt is Asymmetric or Hidden Asymmetric"))
+	TArray<AMonster*> TraitorMonsters;
+#pragma endregion
+
+#pragma region Survivor Properties
+	UPROPERTY(EditAnywhere,
+			BlueprintReadOnly, Category = "Haunt|Survivors",
+			meta=(Tooltip = "Survivors will always have an objective"))
+	FDataTableRowHandle SurvivorObjective;
+#pragma endregion 
+	
+public:
+	// TODO: Add Constructor
+	// TODO: Add cutscene references(start haunt, end haunt, traitor picked)
+	
+	UFUNCTION(Blueprintable, Category = "Haunt")
+	void StartHaunt();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Haunt")
+	void OnHauntStart();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Haunt")
+	void OnHauntEnd();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Haunt")
+	void OnTraitorPicked(APlayerCharacter* TraitorPlayer);
+
 	
 };
